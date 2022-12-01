69811c4a83e1f78453fc568e9cf56550df757a11
Bernardo Fernández <andres_bernardo15@hotmail.com>
2022-12-01 10:49:44 -0300

    verify weight

---
+++ main.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import sys
+
+def random_int_to_n(n):
+    return np.random.randint(n)
+
+def random_0_to_1():
+    return np.random.random()
+
+def ruleta(c):
+    aux = np.arange(1,c+1)**-tau
+    aux /= np.sum(aux)
+    aux = np.cumsum(aux)
+    print(aux)
+    return aux
+
+def verify_weight(solucion):
+    if np.sum(solucion*elements[:,1]) <= c:
+        return True
+    else:
+        return False
+
+# py .\main.py knapPI_1_50_100000.csv 10 10 1.4 
+
+if len(sys.argv) == 5:
+   archivo_entrada = sys.argv[1]
+   seed = int(sys.argv[2])
+   iteraciones = int(sys.argv[3])
+   tau = float(sys.argv[4])
+else:
+   print('Formato de argumentos ingresados no es válido: <Nombre archivo de entrada> <Valor semilla> <Número de iteraciones> <Valor de Tau>.')
+   exit()
+
+np.random.seed(seed=seed)
+test = np.genfromtxt(archivo_entrada, delimiter=" ", skip_header=1,max_rows=2,dtype=int,usecols=(1))
+n = test[0]
+c = test[1]
+
+elements = np.genfromtxt(archivo_entrada, delimiter=",",skip_header=5,max_rows=n,dtype=int,usecols=(0,1,2))
+
+print("n:",n," c:",c)
+Ruleta = ruleta(n)
+
+solucion = np.random.randint(2,size=n)
+
+print(verify_weight(solucion))
